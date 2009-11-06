212e87b454f687b9fb3202eadbd99fa160ff436d
Matthias Rampke <matthias@2pktfkt.de>
2009-11-06 20:56:00 +0100

    python 2.5 compatibility

--- crawluser.py
+++ crawluser.py
@@ -6,19 +6,19 @@ import sys
 
 user = "plomlompom"
 tweets_per_call = 200
-api_url_initial = "http://twitter.com/statuses/user_timeline.json?screen_name={screen_name}&count={count}"
-api_url = api_url_initial + "&max_id={max_id}"
+api_url_initial = "http://twitter.com/statuses/user_timeline.json?screen_name=%(screen_name)s&count=%(count)i"
+api_url = api_url_initial + "&max_id=%(max_id)i"
 
 statuses = []
 
-request = urllib.urlopen(str.format(api_url_initial,screen_name=user,count=tweets_per_call))
+request = urllib.urlopen(api_url_initial % {"screen_name":user, "count":tweets_per_call})
 
 while request.getcode() == 200:
   response = json.loads(request.read())
   for status in response:
     print status["created_at"], status["text"].encode('utf-8')
   statuses.append(response)
-  request = urllib.urlopen(str.format(api_url,screen_name=user,count=tweets_per_call,max_id=response[-1]["id"]-1))
+  request = urllib.urlopen(api_url % {"screen_name":user, "count":tweets_per_call,"max_id":response[-1]["id"]-1})
 else:
   print >> sys.stderr, "HTTP Error " + str(request.getcode()) + " on URL " + request.geturl()
   exit(1)
