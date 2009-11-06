f8c51a47d8b322530d33338f14ea22273829ce2d
Matthias Rampke <matthias@rampke.de>
2009-11-06 14:28:56 +0100

    fix json object syntax

--- crawluser.py
+++ crawluser.py
@@ -13,11 +13,11 @@ statuses = []
 request = urllib.urlopen(str.format(api_url_initial,screen_name=user,count=tweets_per_call))
 
 while request.getcode() == 200:
-  response = json.decode(request.read())
-  for status in response.statuses:
-    print status.created_at, status.text
-  statuses.append(response.statuses)
-  request = urllib.urlopen(str.format(api_url,screen_name=user,count=tweets_per_call,max_id=response.statuses[-1].id-1))
+  response = json.loads(request.read())
+  for status in response:
+    print status["created_at"], status["text"]
+  statuses.append(response)
+  request = urllib.urlopen(str.format(api_url,screen_name=user,count=tweets_per_call,max_id=response[-1]["id"]-1))
 else:
   printf(stderr, "HTTP Error " + request.getcode() + " on URL " + request.geturl())
   exit(1)
